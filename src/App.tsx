a0fa553c7ed8a8d6353139d389ff4d7c492de58b
slicycode <deparisjules180@gmail.com>
2024-01-21 11:51:31 +0100

    feat: add header and custom utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { GitHubBanner, Refine, WelcomePage } from '@refinedev/core'
+import {
+  Authenticated,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from '@refinedev/core'
 import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools'
 import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar'
 
 import { useNotificationProvider } from '@refinedev/antd'
 import '@refinedev/antd/dist/reset.css'
 
-import { Home, ForgotPassword, Login, Register } from './pages'
+import { ForgotPassword, Home, Login, Register } from './pages'
 
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from '@refinedev/react-router-v6'
 import { App as AntdApp } from 'antd'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import Layout from './components/layout'
 import { authProvider, dataProvider, liveProvider } from './providers'
 
 function App() {
@@ -37,11 +44,23 @@ function App() {
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
-                <Route index element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<Home />} />
+                </Route>
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
